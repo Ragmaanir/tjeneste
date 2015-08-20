f0a16b661a676048bc45f8ece956a6003a133c5f
Lasse Fabisch <ragmaanir@gmail.com>
2015-08-20 11:59:24 +0200

    Very simple but working router

--- spec/router_spec.cr
+++ spec/router_spec.cr
@@ -1,11 +1,15 @@
 require "./spec_helper"
 
 describe Tjeneste::Router do
-  Node = Tjeneste::Router::Node
+
+  NODE = Tjeneste::Router::Node
+  PathMatcher = Tjeneste::Router::PathMatcher
+  VerbMatcher = Tjeneste::Router::VerbMatcher
+
   it "" do
-    root = Node.new(children: [
-      Node.new(segment: "users"),
-      Node.new(segment: "topics")
+    root = NODE.new(matchers: [PathMatcher.new("/")], children: [
+      NODE.new(matchers: [PathMatcher.new("users")]),
+      NODE.new(matchers: [PathMatcher.new("topics")])
     ])
     router = Tjeneste::Router.new(root)
 
@@ -17,5 +21,49 @@ describe Tjeneste::Router do
     when nil then fail("route was nil")
     when Tjeneste::Router::Route then assert route.path == [root, root.children.first]
     end
+
+    req = HTTP::Request.new("GET", "not_found")
+
+    route = router.route(req)
+
+    assert route == nil
   end
+
+  it "matches nested paths" do
+    root = NODE.new(matchers: [PathMatcher.new("/")], children: [
+      NODE.new(matchers: [PathMatcher.new("users/")], children: [
+        NODE.new(matchers: [PathMatcher.new("special/")], children: [
+          NODE.new(matchers: [PathMatcher.new(:int)])
+        ])
+      ])
+    ])
+    router = Tjeneste::Router.new(root)
+
+    req = HTTP::Request.new("GET", "users/special/1")
+
+    route = router.route(req)
+
+    assert route
+  end
+
+  it "matches HTTP verbs" do
+    root = NODE.new(matchers: [PathMatcher.new("/")], children: [
+      NODE.new(matchers: [PathMatcher.new("users/"), VerbMatcher.new(Tjeneste::Router::Verb::POST)])
+    ])
+    router = Tjeneste::Router.new(root)
+
+    req = HTTP::Request.new("POST", "users/special/1")
+
+    route = router.route(req)
+
+    assert route
+
+    #
+    req = HTTP::Request.new("GET", "users/special/1")
+
+    route = router.route(req)
+
+    assert !route
+  end
+
 end
--- src/tjeneste/router.cr
+++ src/tjeneste/router.cr
@@ -1,9 +1,122 @@
 module Tjeneste
   class Router
-    class Node
-      property :parent, :children, :segment
 
-      def initialize(@children = [] of Node, @segment = nil : String?)
+    enum Verb
+      GET     = 1
+      POST    = 2
+      PUT     = 3
+      DELETE  = 4
+      HEAD    = 5
+    end
+
+    class RequestState
+      property :request, :path_index
+
+      def initialize(@request : HTTP::Request, @path_index = 0)
+      end
+
+      def initialize(request : RequestState, @path_index = 0)
+        @request = request.request
+      end
+
+      def path
+        request.path[path_index..-1]
+      end
+    end
+
+    abstract class MatchResult
+      abstract def success? : Bool
+    end
+
+    class MatchSuccess < MatchResult
+      property :request
+
+      def initialize(@request : RequestState)
+      end
+
+      def success?
+        true
+      end
+    end
+
+    class MatchFailure < MatchResult
+      property :error
+
+      def initialize(@error)
+      end
+
+      def success?
+        false
+      end
+    end
+
+    abstract class Matcher
+      abstract def match(request : RequestState) : RequestState?
+    end
+
+    class PathMatcher < Matcher
+
+      PREDEFINED_MATCHERS = {
+        int: /\A\d+/
+      }
+
+      def initialize(@matcher)
+      end
+
+      def match(request : RequestState)
+        case @matcher
+          when String then match_internally(@matcher as String, request)
+          when Regex then match_internally(@matcher as Regex, request)
+          when Symbol then match_internally(@matcher as Symbol, request)
+          else raise "Unhandled matcher type #{@matcher.class}"
+        end
+      end
+
+      private def match_internally(matcher : String, request : RequestState) : MatchResult
+        if request.path.starts_with?(matcher)
+          MatchSuccess.new(RequestState.new(request, request.path_index + matcher.length))
+        else
+          MatchFailure.new("#{matcher} != #{request.path}")
+        end
+      end
+
+      private def match_internally(matcher : Regex, request : RequestState) : MatchResult
+        if m = matcher.match(request.path)
+          MatchSuccess.new(RequestState.new(request, request.path_index + m.length))
+        else
+          MatchFailure.new("#{matcher.source} != #{request.path}")
+        end
+      end
+
+      private def match_internally(matcher : Symbol, request : RequestState) : MatchResult
+        predef = PREDEFINED_MATCHERS[matcher]
+        if m = predef.match(request.path)
+          MatchSuccess.new(RequestState.new(request, request.path_index + m.length))
+        else
+          MatchFailure.new("#{predef.source} != #{request.path}")
+        end
+      end
+    end
+
+    class VerbMatcher < Matcher
+      property :verb
+
+      def initialize(@verb : Verb)
+      end
+
+      def match(request : RequestState)
+        if verb.to_s == request.request.method
+          MatchSuccess.new(request)
+        else
+          MatchFailure.new("FAILURE: #{verb} != #{request.request.method}")
+        end
+      end
+    end
+
+    class Node
+      property :parent, :children, :matchers
+
+      def initialize(@matchers = [] of Matcher, @children = [] of Node)
         @children.each{ |c| c.parent = self }
       end
 
@@ -13,6 +126,20 @@ module Tjeneste
 
       def leaf?
         children.empty?
+      end
+
+      def match(request : RequestState) : RequestState?
+        results = [] of MatchResult
+
+        all_match = matchers.all? do |m|
+          res = m.match(request)
+          results << res
+          case res
+            when MatchSuccess then request = res.request
+          end
+        end
+
+        request if all_match
       end
 
       #def inspect
@@ -27,21 +154,21 @@ module Tjeneste
       end
     end
 
-    getter :root
+    getter :root, :logger
 
-    def initialize(@root)
+    def initialize(@root, @logger = Logger.new(STDOUT) : Logger)
     end
 
     def route(request) : Route?
       node = root
       node_path = [node]
-      segments = request.path.split('/')
+      req = RequestState.new(request)
 
       while !node.leaf?
-        segment = segments.shift
-
         next_node = node.children.find do |c|
-          c.segment == segment
+          if res = c.match(req)
+            req = res
+          end
         end
 
         if next_node
@@ -54,5 +181,6 @@ module Tjeneste
 
       Route.new(node_path) if node.leaf?
     end
+
   end
 end
