2842a3f17ec8eb6e7551954507cb3cd8477e9360
Lasse Fabisch <ragmaanir@gmail.com>
2015-08-21 19:30:25 +0200

    Use nested NodeBuilders instead of complicated stack-of-arrays implementation

--- src/tjeneste/routing/router_builder.cr
+++ src/tjeneste/routing/router_builder.cr
@@ -2,27 +2,63 @@ module Tjeneste
   module Routing
     class RouterBuilder
 
-      include BuilderDSL
+      # Actually builds just the array of children and returns them as :result
+      class NodeBuilder
+        getter :result
 
-      def self.build(&block : RouterBuilder -> Nil) : Router
+        def self.build(block : NodeBuilder -> Nil)
+          new(block).result
+        end
+
+        def initialize(block : NodeBuilder -> Nil)
+          @result = [] of Node
+          block.call(self)
+        end
+
+        def path(name, &block : NodeBuilder -> Nil) : Nil
+          children = NodeBuilder.build(block)
+
+          @result << Node.new(
+            matchers: [PathMatcher.new("#{name}/")],
+            children: children
+          )
+          nil
+        end
+
+        def get(name, block : HttpContext -> Nil) : Nil
+          action(Verb::GET, name, block)
+        end
+
+        def post(name, block : HttpContext -> Nil) : Nil
+          action(Verb::POST, name, block)
+        end
+
+        private def action(verb : Verb, name, block : HttpContext -> Nil) : Nil
+          node = Node.new(
+            matchers: [PathMatcher.new(name), VerbMatcher.new(verb)],
+            action: block
+          )
+          @result << node
+          nil
+        end
+      end
+
+      def self.build(&block : NodeBuilder -> Nil) : Router
         router = new(block).result
         router as Router
       end
 
       getter :result
 
-      @node_stack = [[] of Node]
-
-      def initialize(block : RouterBuilder -> Nil)
-        @node_stack = [[] of Node]
-
-        path("") do |r|
-          block.call(self)
-        end
-
-        root_node = @node_stack.first.last
+      def initialize(block : NodeBuilder -> Nil)
+        children = NodeBuilder.build(block)
+        root_node = Node.new(
+          matchers: [PathMatcher.new("/")],
+          children: children
+        )
         @result = Router.new(root_node)
       end
+
     end
   end
 end
