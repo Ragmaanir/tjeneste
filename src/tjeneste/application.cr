79ed52ca8c41a9e86c306c93491f895a9867e769
Ragmaanir <ragmaanir@gmail.com>
2017-04-22 05:06:49 +0200

    Initialize application correctly

--- src/tjeneste/application.cr
+++ src/tjeneste/application.cr
@@ -5,14 +5,17 @@ require "./routing/**"
 module Tjeneste
   abstract class Application
     getter logger : Logger
-    getter middleware_stack : HttpBlock
+
+    @middleware_stack : HttpBlock?
 
     def initialize(@port : Int32 = 3000, @logger = Logger.new(STDOUT))
-      @middleware_stack = build_middleware
-
       @server = Tjeneste::Server.new(@port, @logger) do |ctx|
-        @middleware_stack.call(ctx)
+        middleware_stack.call(ctx)
       end
+    end
+
+    def middleware_stack
+      @middleware_stack ||= build_middleware
     end
 
     abstract def build_middleware : HttpBlock
